927755bf994c8bce037fca12d81523be39e3466f
Gaz Aston <gaz@dxw.com>
2020-03-03 17:22:22 +0000

    send admins to dashboard
    - if a username is ‘admin’, send them to /dashboard
    - adds a current user component

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,16 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.post('/authenticate', function (req, res) {
+  // Get the answer from session data
+  // The name between the quotes is the same as the 'name' attribute on the input elements
+  // However in JavaScript we can't use hyphens in variable names
+
+  if (req.session.data['username'] == 'admin') {
+    res.redirect('dashboard')
+  } else {
+    res.redirect('/')
+  }
+})
+
 module.exports = router
